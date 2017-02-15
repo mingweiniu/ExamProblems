3dc5611f17c5a0be47e2e0ebb8f30dac969f5f28
mingweiniu <mingweiniu@gmail.com>
2017-02-15 20:12:34 +0800

    leet code test case

---
+++ LeetCode/TestCase.cpp
@@ -0,0 +1,34 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif //for VS14
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+
+//#include "104.hpp"
+
+int main(int argc, char **argv) {
+	/*
+	Solution sol{};
+	std::clock_t start_clock, end_clock;
+
+	start_clock = std::clock();
+	for (int i = 0;i<5000;++i)
+		sol.MySolution(nullptr);
+	end_clock = std::clock();
+	std::cout << "mytime = " << end_clock - start_clock << '\n';
+
+
+	start_clock = std::clock();
+	for (int i = 0;i<5000;++i)
+		//sol.OtherSolution(sol.test_arg1, sol.test_arg2);
+		end_clock = std::clock();
+	std::cout << "other = " << end_clock - start_clock << '\n';
+	*/
+	system("PAUSE");
+	return 0;
+}
