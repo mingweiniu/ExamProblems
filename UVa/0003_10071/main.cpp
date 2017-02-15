b086886ba17b4a4a1d4a0fb31e71a3103acaaf8c
Sidney Niu <mingweiniu@gmail.com>
2017-02-15 20:21:49 +0800

    Create main.cpp

---
+++ UVa/0003_10071/main.cpp
@@ -0,0 +1,16 @@
+// wtf?
+// This problem metioned about acceleration without using it in solution
+// How ridiculous it is.
+
+#include <iostream>
+
+int main ()
+{
+  int velocity,time;
+  while(std::cin >> velocity)
+  {
+      std::cin >> time;
+      std::cout << 2 * velocity * time << '\n' ;
+  }
+
+}
