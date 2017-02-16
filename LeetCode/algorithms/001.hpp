1099c07b6e8469a34218846a88faadb70b3ead48
Sidney Niu <mingweiniu@gmail.com>
2017-02-16 08:15:06 +0800

    Create 001.hpp

---
+++ LeetCode/algorithms/001.hpp
@@ -0,0 +1,25 @@
+//1. Two Sum
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //O(n) algorithm
+        auto n = nums.size();
+        std::unordered_map<int,decltype(n)> input;
+        for(int i=0; i <n; ++i ) input[nums[i]] = i;
+        
+        std::vector<int> output;
+        for(auto num: nums){
+            if( (input[target - num]) && (target != 2* num) ){
+                output.push_back(input[num]);
+                output.push_back(input[target - num]);
+                return output;
+            }
+        }
+        
+        for(int i=0; i<n; ++i)
+            if(target == 2* nums[i]) 
+                output.push_back(i);
+        return output;
+    }
+    
+};
