41dfac9a1f39f0860a0562c33a7171abbd7356d0
Sidney Niu <mingweiniu@gmail.com>
2017-11-09 03:27:32 +0800

    Update ipv4touint32.cpp

--- Algorithms/ipv4touint32.cpp
+++ Algorithms/ipv4touint32.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cstdlib>
 
-std::string ipv4 = "8.8.8.8";
+std::string ipv4 = "31.13.87.36";
 
 
 int main() {
@@ -11,11 +11,11 @@ int main() {
 
     char p[4] = {} ;
     ipv4 += '.';
-    for(int i = 0; i < 4; ++i){
+    for(int i = 0; i <4 ; ++i){
         int n = ipv4.find('.');
         string temp(ipv4.begin(), ipv4.begin() + n);
         ipv4.erase(ipv4.begin(), ipv4.begin() + n + 1);
-        p[i] = atoi(temp.data());
+        p[3-i] = atoi(temp.data());
     }
     cout<< *(reinterpret_cast<unsigned int*>(p)) << '\n';
     
