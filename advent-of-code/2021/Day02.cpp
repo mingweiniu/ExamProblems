009e7731902af9a14e67719d86c47dffca473bb6
mingweiniu <mingweiniu@gmail.com>
2021-12-03 20:38:51 +0800

    Create Day02.cpp

---
+++ advent-of-code/2021/Day02.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <sstream>
+
+#include <vector>
+#include <string>
+
+int main() {
+  unsigned long long horizontal = 0;
+	unsigned long long depth = 0;
+	int aim = 0;
+
+	for (size_t i = 0; i < 1000; i++)
+	{
+		auto direction_str = std::string(inputs[2 * i]);
+		int data = 0;
+
+		std::stringstream ss;
+		ss << std::string(inputs[2 * i + 1]);
+		ss >> data;
+
+		if (direction_str.compare("forward") == 0) 
+		{
+			horizontal += data;
+			depth += aim * data;
+		}
+		else if (direction_str.compare("down") == 0)
+		{
+			aim += data;
+		}
+		else if (direction_str.compare("up") == 0)
+		{
+			aim -= data;
+		}
+
+	}
+
+
+	std::cout << depth  * horizontal ;
+  return 0;
+}
