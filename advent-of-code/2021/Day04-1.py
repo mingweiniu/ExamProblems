51c0e44d4800bf7064501b6ea38640a89d31f101
mingweiniu <mingweiniu@gmail.com>
2021-12-05 01:12:37 +0800

    Update Day04-1.py

--- advent-of-code/2021/Day04-1.py
+++ advent-of-code/2021/Day04-1.py
@@ -7,14 +7,17 @@ class Table:
         table = [[int(i) for i in line.split(' ')] for line in table]
         self.table = np.array(table)
         self.record = np.zeros(self.table.shape, dtype=int)
-        
+        self.already_bingo = False
+
+    def IfAlreadyBingo(self):
+        return self.already_bingo
+
     def IfBingo(self, number):
         pose = np.where(self.table == number)
         self.record[pose] = 1
         if( (self.record.sum(axis=0).tolist().count(5) > 0) or (self.record.sum(axis=1).tolist().count(5) > 0)):
-            return True
-        else:
-            return False
+            self.already_bingo = True
+        return self.already_bingo
 
     def GetAnswer(self, bingo_number):
         self.record = 1 - self.record
@@ -24,36 +27,50 @@ class Table:
        
 
 
-lines_temp = open('input.txt', 'r').readlines()
-Lines = []
-for line in lines_temp:
-	# replace many space to one space
-	Lines.append(' '.join(line.split()))
+
+input_lines = []
+with open('input.txt', 'r') as input_file:
+    for line in input_file:
+	    # replace many space to one space
+	    input_lines.append(' '.join(line.split()))
 
 # data for each round
-play_rounds = [int(i) for i in Lines[0].split(',')]
-Lines = Lines[1:]
+play_rounds = [int(i) for i in input_lines[0].split(',')]
+input_lines = input_lines[1:]
 
 # create tables
 list_tables = []
-while Lines:
-    list_tables.append(Table(Lines[:6]))
-    Lines = Lines[6:]
+while input_lines:
+    list_tables.append(Table(input_lines[:6]))
+    input_lines = input_lines[6:]
 
 
-# play game
+# play part1 game
 list_bingo_table = []
-bingo_number = -1
+bingo_numbers = []
 for current_round in play_rounds:
     if list_bingo_table:
         break
-    else:
-        bingo_number = current_round
-        for table in list_tables:
-            res = table.IfBingo(current_round)
-            if(res):
+
+    for table in list_tables:
+        if not table.IfAlreadyBingo():
+            if(table.IfBingo(current_round)):
                 list_bingo_table.append(table)
+                bingo_numbers.append(current_round)
 
 
+print(str(list_bingo_table[-1].GetAnswer(bingo_numbers[-1])))
 
-print(str(list_bingo_table[0].GetAnswer(bingo_number)))
+# play part2 game
+list_bingo_table = []
+bingo_numbers = []
+for current_round in play_rounds:
+
+    for table in list_tables:
+        if not table.IfAlreadyBingo():
+            if(table.IfBingo(current_round)):
+                list_bingo_table.append(table)
+                bingo_numbers.append(current_round)
+
+
+print(str(list_bingo_table[-1].GetAnswer(bingo_numbers[-1])))
