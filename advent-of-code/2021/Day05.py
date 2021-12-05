2273ca599c6bca92685a71045ea314fe75e68281
mingweiniu <mingweiniu@gmail.com>
2021-12-05 14:25:59 +0800

    Update Day05.py

--- advent-of-code/2021/Day05.py
+++ advent-of-code/2021/Day05.py
@@ -14,7 +14,6 @@ max_num = line_pairs.max() + 1
 terrain = np.zeros([max_num, max_num])
 
 for pair in line_pairs:
-
     start_point, end_point = pair 
 
     if start_point[0] == end_point[0]:
@@ -29,9 +28,17 @@ for pair in line_pairs:
         b = end_point[0]
         for y in range(min(a, b), max(a, b) + 1):
             terrain[start_point[1]][y] +=1
+    pass
+
+print((np.asarray(terrain) > 1).sum())
 
 
-    # part 2    
+# part 2
+for pair in line_pairs:
+    start_point, end_point = pair 
+    
+    # base on part 1 is done
+    # only add diagonal line
     vec = start_point - end_point
     if abs(vec[0]) == abs(vec[1]):
         steps = abs(vec[0])
@@ -39,7 +46,6 @@ for pair in line_pairs:
         for t in range(0, steps + 1):
             target = start_point + t * ont_step
             terrain[target[1]][target[0]] +=1
-
 
     pass
 
